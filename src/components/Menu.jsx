547b0c64de3e237dd07151330b731d3a85c80483
joaomonteir0 <joao.mont@ua.pt>
2025-03-20 12:52:59 +0000

    basic routing

---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+export default function Menu() {
+  return (
+    <div className="w-screen h-auto bg-black">
+        <h1>MENU</h1>
+    </div>
+  );
+}
---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+const Register = () => {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen">
+        <h1 className="text-2xl font-bold">Register Page</h1>
+        
+      </div>
+    );
+  };
+  
+  export default Register;
+  
